95441b74b7e0f724eb7aefaa60fa65b61ccc8376
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-03-06 13:55:39 -0500

    Update StarterFile1.java

--- StarterFile1.java
+++ StarterFile1.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class College{
 	// implement our UML Diagram:
 	public static void main(String [] args){
-		System.out.println("HELLO");
+		System.out.println("Starting Program");
+		TestDriver tester = new TestDriver();
+		tester.TestHarness();
 
+		System.out.println("End of Program");
 	}
 }
 
@@ -21,19 +26,31 @@ class GPA{
 	private String studentID;
 	private String Term;
 	private float GPA;
+	public GPA(String sid, String _term) {
+		this.studentID = sid;
+		this.Term = _term;
+	}
 }
 
 class Group{
-	private Vector GroupName;
+	private Vector GroupName = new Vector();
 	private String Term;
+	public Vector getGroupName(){
+		//#TODO write a msg to logfile when accessed 
+		return GroupName;
+	}
 }
 
 class TestDriver{
-	public void TestMethod(){
+	public void TestHarness(){
 		// test procedure:
 		// make at least one student object
 		// insert it into the Vector called GroupName
 
 		Student a = new Student("Joe","C001");
+		Group g = new Group();
+		Vector temp = g.getGroupName();
+		temp.addElement(a);
+		//#TODO implement code to read/write filesystem: import a text file to populate our vector
 	}
 }
